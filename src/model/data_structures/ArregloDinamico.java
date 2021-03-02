2741eeca1f30d914f979fe76f69d1d1e4de3f197
repl.it user <replituser@example.com>
2021-03-02 03:04:22 +0000

    actualizacion

--- src/model/data_structures/ArregloDinamico.java
+++ src/model/data_structures/ArregloDinamico.java
@@ -4,211 +4,190 @@ import java.util.Collection;
 
 import javax.lang.model.util.ElementScanner6;
 
+import model.logic.YoutubeVideo;
+
 /**
- * 2019-01-23
- * Estructura de Datos Arreglo Dinamico de Strings.
- * El arreglo al llenarse (llegar a su maxima capacidad) debe aumentar su capacidad.
+ * 2019-01-23 Estructura de Datos Arreglo Dinamico de Strings. El arreglo al
+ * llenarse (llegar a su maxima capacidad) debe aumentar su capacidad.
+ * 
  * @author Fernando De la Rosa
  *
  */
-public class ArregloDinamico<T extends Comparable<T>>implements ILista<T> 
-{
-	/**
-	 * Capacidad maxima del arreglo
-	 */
-	private int tamanoMax;
-	/**
-	 * Numero de elementos presentes en el arreglo (de forma compacta desde la posicion 0)
-	 */
-	private int tamanoAct;
-	/**
-	 * Arreglo de elementos de tamaNo maximo
-	 */
-	private T elementos[ ];
+public class ArregloDinamico<T extends Comparable<T>> implements ILista<T> {
+  /**
+   * Capacidad maxima del arreglo
+   */
+  private int tamanoMax;
+  /**
+   * Numero de elementos presentes en el arreglo (de forma compacta desde la
+   * posicion 0)
+   */
+  private int tamanoAct;
+  /**
+   * Arreglo de elementos de tamaNo maximo
+   */
+  private T elementos[];
 
-	/**
-	 * Construir un arreglo con la capacidad maxima inicial.
-	 * @param max Capacidad maxima inicial
-	 */
-	public ArregloDinamico( int max )
-	{
-		elementos = (T[])new Object[max];
-		tamanoMax = max;
-		tamanoAct = 0;
-	}
+  /**
+   * Construir un arreglo con la capacidad maxima inicial.
+   * 
+   * @param max Capacidad maxima inicial
+   */
+  public ArregloDinamico(int max) {
+    elementos = (T[]) new Object[max];
+    tamanoMax = max;
+    tamanoAct = 0;
+  }
 
-	public void agregar( T dato )
-	{
-		if ( tamanoAct == tamanoMax )
-		{  // caso de arreglo lleno (aumentar tamaNo)
-			tamanoMax = 2 * tamanoMax;
-			T [ ] copia = elementos;
-			elementos = (T[]) new Object[tamanoMax];
-			for ( int i = 0; i < tamanoAct; i++)
-			{
-				elementos[i] = copia[i];
-			} 
-			System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
-		}	
-		elementos[tamanoAct] = dato;
-		tamanoAct++;
-	}
+  public void agregar(T dato) {
+    if (tamanoAct == tamanoMax) { // caso de arreglo lleno (aumentar tamaNo)
+      tamanoMax = 2 * tamanoMax;
+      T[] copia = elementos;
+      elementos = (T[]) new Object[tamanoMax];
+      for (int i = 0; i < tamanoAct; i++) {
+        elementos[i] = copia[i];
+      }
+      System.out.println("Arreglo lleno: " + tamanoAct + " - Arreglo duplicado: " + tamanoMax);
+    }
+    elementos[tamanoAct] = dato;
+    tamanoAct++;
+  }
 
-	public int darCapacidad() 
-	{
-		return tamanoMax;
-	}
+  public int darCapacidad() {
+    return tamanoMax;
+  }
 
-	public int darTamano() 
-	{
-		return tamanoAct;
-	}
+  public int darTamano() {
+    return tamanoAct;
+  }
 
-	public T darElemento(int i) 
-	{
-		return elementos[i];
-	}
+  public T darElemento(int i) {
+    return elementos[i];
+  }
 
-	public T[] darElementos()
-	{
-		return elementos;
-	}
+  public T[] darElementos() {
+    return elementos;
+  }
 
-	public T buscar(T dato)
-	{
-		T solucion = null;
-		int i = 0;
-		boolean encontro = false;
-		while (i < tamanoAct && !encontro)
-		{
-			if(elementos[i].compareTo(dato) == 0)
-			{
-				encontro = true;
-				solucion = elementos[i];
-			}
-			i++;
-		}
-		return solucion;
-	}
+  public T buscar(T dato) {
+    T solucion = null;
+    int i = 0;
+    boolean encontro = false;
+    while (i < tamanoAct && !encontro) {
+      if (elementos[i].compareTo(dato) == 0) {
+        encontro = true;
+        solucion = elementos[i];
+      }
+      i++;
+    }
+    return solucion;
+  }
 
-	public void eliminar(T dato) 
-	{
-		for(int i = 0; i < tamanoAct; i++)
-		{
-			if(elementos[i].compareTo(dato) == 0)
-			{
-				if(i == tamanoAct)
-				{
-					tamanoAct --;
-				}
-				else
-				{
-					for(int j = i + 1; j < tamanoAct; j++)
-					{
-						elementos[i] = elementos[j];							
-					}
-					tamanoAct--;
-				}
-			}
-		}
-	}
+  public void eliminar(T dato) {
+    for (int i = 0; i < tamanoAct; i++) {
+      if (elementos[i].compareTo(dato) == 0) {
+        if (i == tamanoAct) {
+          tamanoAct--;
+        } else {
+          for (int j = i + 1; j < tamanoAct; j++) {
+            elementos[i] = elementos[j];
+          }
+          tamanoAct--;
+        }
+      }
+    }
+  }
 
-	public void invertir( )
-	{
-		T temporal[] = (T[]) new Object[tamanoMax];
-		int j = 0;
-		for(int i = tamanoAct; i >= 0; i--)
-		{
-			temporal[j] = elementos[i];
-			j++;
-		}
-		elementos = temporal;
-	}
-	public void addFirst(T element) 
-	{
-		elementos[0] = element;
-	}
+  public void invertir() {
+    T temporal[] = (T[]) new Object[tamanoMax];
+    int j = 0;
+    for (int i = tamanoAct; i >= 0; i--) {
+      temporal[j] = elementos[i];
+      j++;
+    }
+    elementos = temporal;
+  }
 
-	public void addLast(T element) 
-	{
-		elementos[(elementos.length-1)] = element;
-	}
+  public void addFirst(T element) {
+    elementos[0] = element;
+  }
 
-	public void insertElement(T element, int pos) 
-	{
-		elementos[pos] = element;
-	}
+  public void addLast(T element) {
+    elementos[(elementos.length - 1)] = element;
+  }
 
-	public T removeFirst() 
-	{
-		T eliminado = elementos[0];
-		elementos[0] = null;
-		return eliminado;
-	}
+  public void insertElement(T element, int pos) {
+    elementos[pos] = element;
+  }
 
-	public T removeLast() 
-	{
-		T eliminado = elementos[(elementos.length-1)];
-		elementos[(elementos.length-1)] = null;
-		return eliminado;
-	}
+  public T removeFirst() {
+    T eliminado = elementos[0];
+    elementos[0] = null;
+    return eliminado;
+  }
 
-	public T deleteElement(int pos) 
-	{
-		T eliminado = elementos[pos];
-		elementos[pos] = null;
-		return eliminado;
-	}
+  public T removeLast() {
+    T eliminado = elementos[(elementos.length - 1)];
+    elementos[(elementos.length - 1)] = null;
+    return eliminado;
+  }
 
-	public T firstElement() 
-	{
-		return elementos[0];
-	}
+  public T deleteElement(int pos) {
+    T eliminado = elementos[pos];
+    elementos[pos] = null;
+    return eliminado;
+  }
 
-	public T lastElement() 
-	{
-		return elementos[(elementos.length-1)];
-	}
+  public T firstElement() {
+    return elementos[0];
+  }
 
-	public T getElement(int pos) 
-	{
-		return elementos[pos];
-	}
+  public T lastElement() {
+    return elementos[(elementos.length - 1)];
+  }
 
-	public int size() 
-	{
-		return elementos.length;
-	}
+  public T getElement(int pos) {
+    return elementos[pos];
+  }
 
-	public boolean isEmpty() 
-	{
-		return elementos.length==0?true:false;
-	}
+  public int size() {
+    return elementos.length;
+  }
 
-	public int isPresent(T element) 
-	{
-		int i = 0;
-		while(i<size())
-		{
-			if(elementos[i]==element)
-			{
-				return i;
-			}
-			else 
-				i++;
-		}
-		return -1;
-	}
+  public boolean isEmpty() {
+    return elementos.length == 0 ? true : false;
+  }
 
-	public void exchange(int pos1, int pos2) 
-	{
-		T temp = elementos[pos1];
-		elementos[pos1] = elementos[pos2];
-		elementos[pos2] = temp;
-	}
+  public int isPresent(T element) {
+    int i = 0;
+    while (i < size()) {
+      if (elementos[i] == element) {
+        return i;
+      } else
+        i++;
+    }
+    return -1;
+  }
 
-	public void changeInfo(int pos, T elem) 
-	{
-		elementos[pos] = elem; 
-	}
+  public void exchange(int pos1, int pos2) {
+    T temp = elementos[pos1];
+    elementos[pos1] = elementos[pos2];
+    elementos[pos2] = temp;
+  }
+
+  public void changeInfo(int pos, T elem) {
+    elementos[pos] = elem;
+  }
+
+  public void cargar(String ruta) {
+    File f = new File(ruta);
+    FileReader fr = new FileReader(f);
+    BufferedReader br = new BufferedReader(fr);
+    String linea = br.readline();
+    while (linea != null) {
+      String[] info = linea.split(",");
+      YoutubeVideo vid = new YoutubeVideo(info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7], info[8], info[9], info[10], info[11], info[12], info[13], info[14], info[15], info[16]);
+      this.agregar(vid);
+    }
+  }
 }
--- src/model/data_structures/ILista.java
+++ src/model/data_structures/ILista.java
@@ -88,4 +88,6 @@ public interface ILista <T extends Comparable<T>>
 	void changeInfo (int pos, T elem);
 	
   public ILista sublista(int numElementos);
+
+  void cargar(String ruta);
 }
